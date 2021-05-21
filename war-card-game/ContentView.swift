7c9d349d0cc61a639ca4974b6ad3af5ec6168623
Peter-32 <peter@impactradius.com>
2021-05-21 00:31:19 -0700

    init

---
+++ war-card-game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  war-card-game
+//
+//  Created by Peter Myers on 5/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
